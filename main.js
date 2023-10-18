d7c3d8fd16beb5edabd8d7932f478cc172a31358
DanisWalker <dani.adm.002@gmail.com>
2023-10-18 17:05:45 +0200

    Button hover done

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
-var button = document.getElementById("checkedButton")
-var checkbox = document.getElementById("checkerChechbox")
-var inputDiv = document.getElementById("Input")
-var database = document.getElementById("databaseError")
-var followup = document.getElementById("followUp")
+const button = document.getElementById("checkedButton")
+const checkbox = document.getElementById("checkerChechbox")
+const inputDiv = document.getElementById("Input")
+const database = document.getElementById("databaseError")
+const followup = document.getElementById("followUp")
+function handleButtonMouseOver() {
+  button.style.backgroundColor = "#333";
+  button.style.color = "#ccc";
+  button.style.borderColor = "darkblue";
+}
+function handleButtonMouseOut() {
+  button.style.background = "darkgrey";
+  button.style.color = "#ccc";
+  button.style.borderColor = "";
+}
+function basicButtonSetting(){
+  button.style.background = "darkgrey";
+  button.style.color = "#ccc";
+}
 checkbox.addEventListener('change', function() {
-    button.disabled = !checkbox.checked;
-  });
+  button.disabled = !checkbox.checked;
+  if (checkbox.checked) {
+    button.addEventListener("mouseover", handleButtonMouseOver);
+    button.addEventListener("mouseout", handleButtonMouseOut);
+    basicButtonSetting();
+  } else {
+    button.style.backgroundColor = "";
+    button.removeEventListener("mouseover", handleButtonMouseOver);
+    button.removeEventListener("mouseout", handleButtonMouseOut);
+  }
+});
 button.addEventListener('click', function(){
   inputDiv.style.display = "none"  
     database.style.display = "block"
