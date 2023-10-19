289452a2ded9315f6a1e2c1dd866347787098d6c
DanisWalker <dani.adm.002@gmail.com>
2023-10-19 20:57:28 +0200

    Finally really center everything

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const checkbox = document.getElementById("checkerChechbox")
 const inputDiv = document.getElementById("Input")
 const database = document.getElementById("databaseError")
 const followup = document.getElementById("followUp")
+const jokes = document.getElementById("indexJokes")
 function handleButtonMouseOver() {
   button.style.backgroundColor = "#333";
   button.style.color = "#ccc";
@@ -29,13 +30,24 @@ checkbox.addEventListener('change', function() {
     button.removeEventListener("mouseout", handleButtonMouseOut);
   }
 });
+const text = "Sorry no database yet"; // The text to be animated
+  const typingEffect = document.getElementById("databaseError");
+  let charIndex = 0;
+  function type() {
+    if (charIndex < text.length) {
+      typingEffect.textContent += text.charAt(charIndex);
+      charIndex++;
+      setTimeout(type, 100); // Adjust typing speed here (in milliseconds)
+    }
+  }
 button.addEventListener('click', function(){
   inputDiv.style.display = "none" ;
-  database.style.display = "block"
-  followup.style.display = "block"
-  database.innerHTML = "404 Database not found";
+  jokes.style.display = "block"
+  type();
+  //database.innerHTML = "404 Database not found";
   console.log('Joke incoming');
   setTimeout(() => {
     followup.innerHTML = "Just kidding I have no database yet"
   }, 3000);
+
 })
